9fb5efbe9580aec193442045c1c145a7731e1203
forest carter <forest.carter@gmail.com>
2018-12-10 12:09:54 -0600

    test1

--- resources/views/pages/cargarshapes.blade.php
+++ resources/views/pages/cargarshapes.blade.php
@@ -15,27 +15,29 @@
         if (env("APP_URL", "somedefaultvalue")=='http://localhost'){
         }else{
             $db = env("DB_PASSWORD", "somedefaultvalue");
-            $loadshp="shp2pgsql -I -s {$srid}:4326 /mnt/c/wamp64/www/lsapp3/public/shp/{$shpfile} {$shapenombre} | PGPASSWORD='{$db}' psql -U postgres -h localhost -d biodiversity3";
+            $loadshp="shp2pgsql -I -s {$srid}:4326 var/www/html/lsapp3/public/shp/{$shpfile} {$shapenombre} | PGPASSWORD='{$db}' psql -U postgres -h localhost -d biodiversity3";
             $output= shell_exec($loadshp);
+            echo $output;
+            echo 'linux';
         }
         //echo '"C:\\Program Files\\PostgreSQL\\10\\bin\\shp2pgsql" -I -s '.$srid.':4326 C:\\wamp64\\www\\lsapp3\\public\\shp\\'.$shpfile.' '.$shapenombre.' | psql -U postgres -d biodiversity3';
 
         //$output= shell_exec('"C:\\Program Files\\PostgreSQL\\10\\bin\\shp2pgsql" -I -s '.$srid.':4326 C:\\wamp64\\www\\lsapp3\\public\\shp\\'.$shpfile.' '.$shapenombre.' | psql -U postgres -d biodiversity3');
 
-        //echo $output;
+       echo shell_exec("ls");
 
         //echo $loadshp;
 
         
         //insert into geom usertable
-        $copyshp="insert into usershapes (nombre, iden_email, geom) values (:nombre, :email, :geom)";
-        $geom= DB::select("select geom from {$shapenombre}", []);
-        $arraytopass=array(
-            ":nombre"=> $shapenombre,
-            ":email"=> session('email'),
-            ":geom"=> $geom[0]->geom,
-        );
-        $results = DB::insert($copyshp, $arraytopass);
+        // $copyshp="insert into usershapes (nombre, iden_email, geom) values (:nombre, :email, :geom)";
+        // $geom= DB::select("select geom from {$shapenombre}", []);
+        // $arraytopass=array(
+        //     ":nombre"=> $shapenombre,
+        //     ":email"=> session('email'),
+        //     ":geom"=> $geom[0]->geom,
+        // );
+        // $results = DB::insert($copyshp, $arraytopass);
         //delete temp table 
         
     }
