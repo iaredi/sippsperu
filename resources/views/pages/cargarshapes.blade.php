742c0ee21de244447bed9a38a6e6c0373b4d5fc0
forest carter <forest.carter@gmail.com>
2018-12-10 16:16:17 -0600

    Upload shape testing

--- resources/views/pages/cargarshapes.blade.php
+++ resources/views/pages/cargarshapes.blade.php
@@ -2,18 +2,43 @@
 @include('inc/php_functions')
 
 <?php
-    if ($_SERVER['REQUEST_METHOD']=="POST") {
+    $errorlist=[];
+    if ($_SERVER['REQUEST_METHOD']=="POST"){
+        $filearray=['shp','shx','dbf','prj'];
+        $base = substr($_FILES['prj']["name"],0,-4);
+        foreach( $filearray as $filetype) {
+            if (!$_FILES[$filetype]["name"]) {
+                $errorlist[]= "Faulta {$filetype} ";
+            }
+            $test = substr($_FILES[$filetype]["name"],0,-4);
+            if (substr($_FILES[$filetype]["name"],-4,4) !== ".{$filetype}" ) {
+                $errorlist[]= "{$filetype} no tiene extension .{$filetype} ";
+            }
+            if (substr($_FILES[$filetype]["name"],0,-4) != $base ) {
+                $message="Todos los cargars debe de tener lo mismo nombre";
+                if (!in_array($message, $errorlist)){
+                    $errorlist[]= $message;
+                }
+            }
+        }
+        if (!$_POST['shapenombre']) {
+            $errorlist[]= "Faulta nombre ";
+        }
+    }
+
+
+    
+    if ($_SERVER['REQUEST_METHOD']=="POST" && sizeof($errorlist)==0 && (!session('visitante'))  ){
         uploadshape('shp');
         uploadshape('shx');
         uploadshape('dbf');
         uploadshape('prj');
         $shpfile=$_FILES['shp']["name"];
-        echo `ogrinfo -al -so ./shp/{$shpfile}`;
-            $sridshell= shell_exec("ogr2ogr -t_srs EPSG:4326 ./shp/{$shpfile} ./shp/{$shpfile}");
-        echo `ogrinfo -al -so ./shp/{$shpfile}`;
-
+        $sridshell= shell_exec("ogr2ogr -t_srs EPSG:4326 ./shp/{$shpfile} ./shp/{$shpfile}");
         $shapenombre=$_POST['shapenombre'];
+        //if (env("APP_ENV", "somedefaultvalue")=='production'){
         if (env("APP_ENV", "somedefaultvalue")=='production'){
+        
             //load to temp table 
             $db = env("DB_PASSWORD", "somedefaultvalue");
             $loadshp="shp2pgsql -I -s 4326:4326 /var/www/html/lsapp3/public/shp/{$shpfile} {$shapenombre} | PGPASSWORD='{$db}' psql -U postgres -h localhost -d biodiversity3";
@@ -35,37 +60,48 @@
             }
         }
     }
+    
 ?>
 
 @include('inc/header')
 @include('inc/nav')
 
  <div class="display: flex p-5 m-5" style="text-align:center;">
-    <div class=" d-inline-flex flex-column justify-content-center" style='width: 350px'>
-    <p class="text-center h5">Cargar Shapes</p>
+    <div class=" d-inline-flex flex-column justify-content-center" style='width: 400px'>
+    <p class="text-center h2">Cargar Shapes</p>
+    <div class=" warnings">
+        <?php
+            if (sizeof($errorlist)>0){
+                foreach ($errorlist as $msg) {
+                    echo "<p class='bg-danger2 text-center'>{$msg}</p>";
+                }
+            }
+        ?>
+    </div>
     <form id="login-form"  method="post" role="form" style="display: block;" enctype="multipart/form-data">
         {{ csrf_field() }}
     <div>
-        <label for="shp" class="shapelabel">.shp</label>
+        <label for="shapenombre" class=" h4 shapenombre">nombre</label>
+        <input type="text" name="shapenombre" id="shapenombre">
+    </div>
+    <div>
+        <label for="shp" class="h4 shapelabel">.shp</label>
         <input type="file" name="shp" id="shp">
     </div>
     <div>
-        <label for="shx" class="shapelabel">.shx</label>
+        <label for="shx" class="h4 shapelabel">.shx</label>
         <input type="file" name="shx" id="shx">
     </div>
     <div>
-        <label for="dbf" class="shapelabel">.dbf</label>
+        <label for="dbf" class="h4 shapelabel">.dbf</label>
         <input type="file" name="dbf" id="dbf">
     </div>
     <div>
-        <label for="prj" class="shapelabel">.prj</label>
+        <label for="prj" class="h4 shapelabel">.prj</label>
         <input type="file" name="prj" id="prj">
     </div>
     
-    <div>
-        <label for="shapenombre" class="shapenombre">nombre</label>
-        <input type="text" name="shapenombre" id="shapenombre">
-    </div>
+    
     <div class="row">
         <input type="submit" name="login-submit" id="login-submit" tabindex="4" class="form-control btn btn-success p-15" value="Enviar">
     </div>
@@ -82,6 +118,7 @@
 
 
 @include('inc/footer')
+
 <?php
     echo '<script>console.log('.json_encode(session('resultofquery')).')</script>';
     session(['resultofquery' => '']);
