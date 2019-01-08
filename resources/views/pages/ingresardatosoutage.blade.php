561b51a3dc5ae7fe78a63987c55b611caf95ce55
forest carter <forest.carter@gmail.com>
2019-01-08 10:18:42 -0600

    ingresar working

---
+++ resources/views/pages/ingresardatosoutage.blade.php
@@ -0,0 +1,7 @@
+@include('inc/php_functions')
+@include('inc/setuppage')
+@include('inc/checkdata')
+@include('inc/savedata')
+@include('inc/header')
+@include('inc/nav')
+<h3>No hay servicio ahora. Regresamos pronto!</h3>
